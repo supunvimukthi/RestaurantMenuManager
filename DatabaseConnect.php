f452a674dda757102333d10c4a8fc22928e582fa
supunvimukthi <supun.15@cse.mrt.ac.lk>
2018-05-17 14:26:03 +0530

    app

--- DatabaseConnect.php
+++ DatabaseConnect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$user = "root";
-$password = "";
-$dbname = "restaurant";
+$servername = "sql12.freemysqlhosting.net";
+$user = "sql12238020";
+$password = "5mKjj9L3rV";
+$dbname = "sql12238020";
 
 // Create connection
 $conn = new mysqli($servername, $user, $password, $dbname);
